dac88c498c93a80f6a9f1ba299cb0765b6aa776e
coltonbegert <begert@ualberta.ca>
2017-02-15 14:56:16 -0700

    get rssi

--- a2/blue.py
+++ a2/blue.py
@@ -5,4 +5,5 @@ nearby_devices = bluetooth.discover_devices(lookup_names=True)
 print("found %d devices" % len(nearby_devices))
 
 for addr, name in nearby_devices:
+    print (nearby_devices)
     print("  %s - %s" % (addr, name))
