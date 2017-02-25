7cb6f073cd5bc11cae1e5dd72d1112392ac03373
coltonbegert <begert@ualberta.ca>
2017-02-25 12:20:38 -0700

    lazy

--- a2/blue2.py
+++ a2/blue2.py
@@ -1,18 +1,24 @@
 
-import bluetooth
+# import bluetooth
+from gattlib import DiscoveryService
 
-target_name = "My Phone"
-target_address = None
+service = DiscoveryService("hci0")
+devices = service.discover(2)
 
-nearby_devices = bluetooth.discover_devices()
-
-for bdaddr in nearby_devices:
-    print bdaddr
-    if target_name == bluetooth.lookup_name( bdaddr ):
-        target_address = bdaddr
-        break
-
-if target_address is not None:
-    print "found target bluetooth device with address ", target_address
-else:
-    print "could not find target bluetooth device nearby"
+# target_name = "My Phone"
+# target_address = None
+#
+# nearby_devices = bluetooth.discover_devices()
+# ble_devices = bluetooth.discover_devices
+# print nearby_devices
+#
+# for bdaddr in nearby_devices:
+#     print bdaddr
+#     if target_name == bluetooth.lookup_name( bdaddr ):
+#         target_address = bdaddr
+#         break
+#
+# if target_address is not None:
+#     print "found target bluetooth device with address ", target_address
+# else:
+#     print "could not find target bluetooth device nearby"
