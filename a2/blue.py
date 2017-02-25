470031216f1780275323fc79329acc2d32001c00
coltonbegert <begert@ualberta.ca>
2017-02-25 13:48:18 -0700

    lazy

--- a2/blue.py
+++ a2/blue.py
@@ -1,14 +1,27 @@
-import bluetooth
+from bluetooth.ble import BeaconService
 
-print("performing inquiry...")
+class Beacon(object):
 
-nearby_devices = bluetooth.discover_devices(
-        duration=8, lookup_names=True, flush_cache=True, lookup_class=False)
+    def __init__(self, data, address):
+        self._uuid = data[0]
+        self._major = data[1]
+        self._minor = data[2]
+        self._power = data[3]
+        self._rssi = data[4]
+        self._address = address
 
-print("found %d devices" % len(nearby_devices))
+    def __str__(self):
+        ret = "Beacon: address:{ADDR} uuid:{UUID} major:{MAJOR}"\
+                " minor:{MINOR} txpower:{POWER} rssi:{RSSI}"\
+                .format(ADDR=self._address, UUID=self._uuid, MAJOR=self._major,
+                        MINOR=self._minor, POWER=self._power, RSSI=self._rssi)
+        return ret
 
-for addr, name in nearby_devices:
-    try:
-        print("  %s - %s" % (addr, name))
-    except UnicodeEncodeError:
-        print("  %s - %s" % (addr, name.encode('utf-8', 'replace')))
+service = BeaconService()
+devices = service.scan(2)
+
+for address, data in list(devices.items()):
+    b = Beacon(data, address)
+    print(b)
+
+print("Done.")
