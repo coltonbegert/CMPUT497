ea7ccd5f80ca2fc0edfb898219d46fba7e0831c6
coltonbegert <begert@ualberta.ca>
2017-02-28 16:41:42 -0700

    reding input from stdin

--- a2/server.py
+++ a2/server.py
@@ -3,6 +3,12 @@
 # www.tutorialspoint.com/python/python_networking.htm
 
 import socket               # Import socket module
+import threading
+import sys
+#
+# def get_input():
+#     message = input()
+#
 
 s = socket.socket()         # Create a socket object
 host = socket.gethostname() # Get local machine name
@@ -16,14 +22,17 @@ while True:
    counter = 0
    while True:
     #   c.send(b'test')
-        counter += 1
+        data = sys.stdin.readlines()
+        if len(data) > 0:
+            c.send(data)
+        # counter += 1
         message = c.recv(1024)
         if not message: break
         if len(message) > 0:
             print(message.decode("ascii"))
-        if counter == 10:
-            counter = 0
-            c.send("buzz bitch")
+        # if counter == 10:
+        #     counter = 0
+        #     c.send("buzz bitch")
 
    # c.send(b'test')
    # print(c.recv(512))
