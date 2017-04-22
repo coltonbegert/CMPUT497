9358a42abd1dc9a4e04404f123395ce759992796
Colton begert <begert@ualberta.ca>
2017-04-21 19:56:05 -0600

    program creates a csv file with accurate measurments

--- Assignment3/ccap.py
+++ Assignment3/ccap.py
@@ -1,6 +1,8 @@
 import smbus
 import sys
 import time
+import subprocess
+import re
 bus = smbus.SMBus(1)
 address = 0x38
    
@@ -15,7 +17,18 @@ def cc2():
 	#print cap
 	return [humid, temp]
 def rssi():
-	return -1
+	command = 'sudo iwlist wlan0 scan'
+	grep_command = 'grep -B 2 "Wifi-RSSI test"'
+	valid_rssi = False
+	while not valid_rssi:
+		process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+		output, error = process.communicate()
+#	print output
+		if "Wifi-RSSI test" in output:
+			valid_rssi = True
+	rssi_pos = output[0:output.find("Wifi-RSSI test")].rfind("Signal level")
+	rssi_val = output[rssi_pos + 13:output.find(" dBm", rssi_pos)]
+	return rssi_val
 def main():
 	if len(sys.argv) > 1:
 		filename = sys.argv[1]
@@ -24,7 +37,7 @@ def main():
 
 	with open(filename, 'w') as f:
 		i = 0
-		while True and i < 10:
+		while True:
 			i += 1
 			cap = cc2()
 			RSSI = rssi()
@@ -32,7 +45,8 @@ def main():
 			output.append(str(time.time()))
 			output.append(str(cap[0]))
 			output.append(str(cap[1]))
-			output.append(str(RSSI))	
+			output.append(RSSI)	
 			f.write(",".join(output) + '\n')
+			print ",".join(output)
 			time.sleep(1)
 main()
